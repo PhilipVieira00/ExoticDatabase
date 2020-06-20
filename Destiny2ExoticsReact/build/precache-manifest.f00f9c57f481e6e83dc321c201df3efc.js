5f1070887f9dbcd67a57dabd1a4a20a7077f243f
Philip Vieira <50379993+PhilipVieira00@users.noreply.github.com>
2020-06-19 20:46:11 -0400

    Manually added files

---
+++ Destiny2ExoticsReact/build/precache-manifest.f00f9c57f481e6e83dc321c201df3efc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d87cb527449ebc349062b62353fa8e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69eb56203ad10647b50d",
+    "url": "/static/css/main.bfd9321d.chunk.css"
+  },
+  {
+    "revision": "ff0571262fbf82fcb288",
+    "url": "/static/js/2.153de447.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.153de447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69eb56203ad10647b50d",
+    "url": "/static/js/main.cf15ab58.chunk.js"
+  },
+  {
+    "revision": "dce1d01262b637418d6a",
+    "url": "/static/js/runtime-main.c913934c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
